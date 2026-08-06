ee2f00c813d357bd41488220050bf4e395e2263a
duphan99 <50976807+duphan99@users.noreply.github.com>
2026-08-06 09:58:31 +0700

    Fix OneSignal Service Worker listener and remove duplicate init call

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
+// Register empty message handler synchronously on initial evaluation to satisfy Chrome 120+ SW requirements
+self.addEventListener('message', () => {});
+
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
